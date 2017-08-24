d6f4b8957f3b40b9d7492ddb1651ddf8412a80f2
gatsunoko <gatsunoko@yahoo.co.jp>
2017-08-25 02:15:08 +0900

    入力したurlから画像ダウンロードできるように

--- app/controllers/gets_controller.rb
+++ app/controllers/gets_controller.rb
@@ -1,4 +1,38 @@
 class GetsController < ApplicationController
+require 'open-uri'
+require 'nokogiri'
+
   def index
   end
+
+  def show
+  end
+
+  def getweb
+    url = params[:url]
+    count = params[:count].to_i
+    1.upto count do |i|
+      img = Nokogiri::HTML.parse(url_set("#{url}/#{i}"), nil, 'utf-8')
+      img.css('img.chapter_img_category2').each do |doc|
+
+        p '-----------------------------------------'
+        p params[:number].to_s + "-" + i.to_s
+        p '-----------------------------------------'
+
+        File.open("public/docs/#{params[:number].to_s}-#{i}.jpeg", 'wb') { |f| f.write(Net::HTTP.get_response(URI.parse(doc[:src])).body) }
+      end
+    end
+
+    render 'show'
+  end
+
+  def url_set(url)
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+    return html
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'gets#index'
   get 'gets/index'
+  get 'gets/show'
+  post 'gets/getweb'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
